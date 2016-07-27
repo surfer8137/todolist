8d49c225f59107257373400752a53b9f7014f8ea
Angel Sanchez <angel@Admins-MacBook-Pro-2.local>
2016-07-27 09:33:04 +0200

    Now tasks are shown by if a task is done or not, email disabled at the moment

--- app.rb
+++ app.rb
@@ -4,34 +4,37 @@ require 'date'
 
 class ToDoListApp < Sinatra::Base
   get '/' do
-    redirect '/0'
+    redirect '/todo'
   end
 
-  get %r{/([-]*[\d]+)} do
-    date =  Date.today
-    days_to_sum = params[:captures].join.to_i
-    date = date + days_to_sum
+  get '/done' do
+    tasks = Task.where(finished: true).sort_by &:finish_time
 
-    more_important_tasks = Task.where(important: true, finish_time: date)
-    less_important_tasks = Task.where(important: false, finish_time: date)
-
-    next_day = days_to_sum + 1
-    previous_day = days_to_sum - 1
-
-    erb :today_tasks, :locals => {
-      important_tasks: more_important_tasks,
-      tasks: less_important_tasks,
-      day: date.day,
-      month: date.mon,
-      year: date.year,
-      next_day: next_day,
-      previous_day: previous_day
+    erb :done_tasks, :locals => {
+      tasks: tasks
     }
+  end
+
+  get '/todo' do
+    date =  Date.today
+
+    more_important_tasks = Task.where(important: true, finished: false).sort_by &:finish_time
+    less_important_tasks = Task.where(important: false, finished: false ).sort_by &:finish_time
+
+    erb :todo_tasks, :locals => {
+      important_tasks: more_important_tasks,
+      tasks: less_important_tasks
+    }
+  end
+
+  get %r{/([\d]+)} do
 
   end
 
   get '/add-task' do
-    erb :add_task
+    erb :add_task, :locals => {
+      date: Date.today.to_s
+    }
   end
 
   post '/add-task' do
@@ -43,10 +46,37 @@ class ToDoListApp < Sinatra::Base
     task = Task.new(
       title: params[:name],
       body: params[:comment],
-      finish_time: Date.today,
-      important: false,
+      finish_time: params[:date],
+      important: important,
       finished: false
     )
     task.save
   end
+
+  def important
+    params[:important] || false
+  end
 end
+
+
+#get %r{/([-]*[\d]+)} do
+#  date =  Date.today
+#  days_to_sum = params[:captures].join.to_i
+#  date = date + days_to_sum
+#
+#  more_important_tasks = Task.where(important: true, finish_time: date)
+#  less_important_tasks = Task.where(important: false, finish_time: date)
+#
+#  next_day = days_to_sum + 1
+#  previous_day = days_to_sum - 1
+#
+#  erb :today_tasks, :locals => {
+#    important_tasks: more_important_tasks,
+#    tasks: less_important_tasks,
+#    day: date.day,
+#    month: date.mon,
+#    year: date.year,
+#    next_day: next_day,
+#    previous_day: previous_day
+#  }
+#end
